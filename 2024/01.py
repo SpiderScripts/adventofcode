988b6f9c0a0ed311436719ec108713b6a22582bc
P Williams <peter@peteandemma.co.uk>
2024-12-03 07:45:05 +0000

    Day 1 original solution and refactor

--- 2024/01.py
+++ 2024/01.py
@@ -1,18 +1,35 @@
 f = open("01input.txt","r")
-
 data = f.readlines()
 
+'''
 data = ["3   4",
 "4   3",
 "2   5",
 "1   3",
 "3   9",
 "3   3"]
+'''
 
-l = sorted([int(a.split("   ")[0]) for a in data])
-r = sorted([int(a.split("   ")[1]) for a in data])
+total = 0
+left = []
+right = []
 
-t1 = sum(abs(l[i] - r[i]) for i in range(len(l)))
-t2 = sum(i*r.count(i) for i in l)
+for i in data:
+    a = i.split("   ")
+    left.append(int(a[0]))
+    right.append(int(a[1]))
 
-print(t1,t2,sep="\n")
+left.sort()
+right.sort()
+
+for i in range(len(left)):
+    total += abs(left[i]-right[i])
+
+print(total)
+
+total2 = 0
+
+for i in range(len(left)):
+    total2+= left[i]*right.count(left[i])
+
+print(total2)
